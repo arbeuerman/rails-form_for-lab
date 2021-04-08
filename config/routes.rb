9dede73861cfd36a22f6feb84daaa85bea18e08a
Alexandra <arbeuerman@gmail.com>
2021-04-08 17:59:08 -0500

    practice with forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :school_classes, only: [:index, :show, :edit, :new, :create, :update]
+  resources :students, only: [:index, :show, :edit, :new, :create, :update]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
